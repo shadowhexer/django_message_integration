c689b3dac6ca0a7f2b6d7684232ff11565ef0cec
shadowhexer <austinmanlangit@gmail.com>
2024-12-08 01:57:55 +0800

    Disabled DB

--- message_integration/message_integration/settings.py
+++ message_integration/message_integration/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'message_integration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': env('DB_ENGINE'),
-        "NAME": env('DB_NAME'),
-        "USER": env('DB_USER'),
-        "PASSWORD": env('DB_PASS'),
-        "HOST": env('DB_HOST'),
-        "PORT": env('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env('DB_ENGINE'),
+#         "NAME": env('DB_NAME'),
+#         "USER": env('DB_USER'),
+#         "PASSWORD": env('DB_PASS'),
+#         "HOST": env('DB_HOST'),
+#         "PORT": env('DB_PORT'),
+#     }
+# }
 
 
 # Password validation
